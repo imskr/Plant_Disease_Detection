c04b390f81bf235e5c122d0958308be98b4302a1
Shubham Kumar <shubhamkrai123@gmail.com>
2019-08-11 13:12:14 +0530

    add models

--- app/server.py
+++ app/server.py
@@ -30,7 +30,7 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name)
+    await download_file(export_file_url, path/export_file_name)
     try:
         learn = load_learner(path, export_file_name)
         return learn
