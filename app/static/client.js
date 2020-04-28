f7386b4f6d277de7ee03aaebb83de23d9a40d6d7
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-04-28 12:34:01 +0530

    fix: UI issue solved (#79)
    * fix: UI issue solved
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * fix: JQueryUI links updated
    
    * feat: analysis button working
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * fix: image show
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * fix: added the Result
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * feat: did prettifing the CSS file
    remove unwanted codes from JS
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * fix: Result element realligned
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * fix: left direction realligned
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>
    
    * fix: top direction
    
    Signed-off-by: Arkadip <in2arkadipb13@gmail.com>

--- app/static/client.js
+++ app/static/client.js
@@ -1,45 +1,60 @@
-var uploadFiles;
-function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
+$(document).ready(function () {
 
-                reader.onload = function (e) {
-                    $('#selected_image')
-                        .attr('src', e.target.result)
-                        .width(220)
-                        .height(220);
+	function readURL(input) {
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
 
-                };
-                alert("Image selected,Click analyse to proceed further");
-                reader.readAsDataURL(input.files[0]);
-                uploadFiles = input.files;
-            }
-        }
-function analyze() {
-	if (uploadFiles == null) {
-		alert("Please select a file to analyze!");
+			reader.onload = function (e) {
+				$('#selected_image')
+					.attr('src', e.target.result)
+					.width(220)
+					.height(220);
+			}
+			reader.readAsDataURL(input.files[0]);
+		}
 	}
-	else{
-	document.getElementById("analyze-button").innerHTML = "Analyzing..";
-	var xhr = new XMLHttpRequest();
-	var loc = window.location;
-	xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
-    xhr.onerror = function() {
-    alert(xhr.responseText);
-  };
-  xhr.responseType = 'json';
- // xhr.setRequestHeader('document');
-     xhr.onload = () => {
-    if (this.readyState === 4) {
-      var response = xhr.response;
-      document.getElementById("result").innerHTML = `Result = ${response["result"]}`;
-    }
-      document.getElementById("analyze-button").innerHTML = "Analyze";
-  };
-  var fileData = new FormData();
-  fileData.append("file", uploadFiles[0]);
-  xhr.send(fileData);
-}
-}
-//}
+
+	$('#imagefile').change(function () {
+		readURL(this);
+	});
+
+
+	$("form#analysis-form").submit(function (event) {
+		event.preventDefault();
+
+		var analyze_button = $("button#analyze-button");
+		var imagefile = $('#imagefile')[0].files;
+
+		if (!imagefile.length > 0) {
+			alert("Please select a file to analyze!");
+		}
+		else {
+			analyze_button.html("Analyzing..");
+			analyze_button.prop("disabled", "true");
+
+			var fd = new FormData();
+			fd.append('file', imagefile[0]);
+
+			var loc = window.location;
+
+			$.ajax({
+				method: 'POST',
+				async: true,
+				url: loc.protocol + '//' + loc.hostname + ':' + loc.port + '/analyze',
+				data: fd,
+				processData: false,
+				contentType: false,
+			}).done(function (data) {
+				console.log("Done Request!");
+				$("#result").html("Result= "+ data.result);
+			}).fail(function (e) {
+				console.log("Fail Request!");
+				console.log(e);
+			});
+		};
+
+		analyze_button.prop("disabled", "");
+		analyze_button.html("Analyze");
+		console.log("Submitted!");
+	});
+});
